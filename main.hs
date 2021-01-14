94b23097203c9cf2a7fa5df644428ab5b7edea74
firedial <42674772+firedial@users.noreply.github.com>
2021-01-14 22:08:37 +0900

    標準形の定義

--- main.hs
+++ main.hs
@@ -58,8 +58,38 @@ getOnePlusList xs (y:ys) = [(xs++((y+1):ys))] ++ getOnePlusList (xs ++ [y]) ys
 isTenpai :: [Int] -> Bool
 isTenpai hai = (countTrue . getAgariHai) hai /= 0
 
+
+isNormalForm :: [Int] -> Bool
+isNormalForm hai = if not (isFormerGravity middleHai reverseMiddleHai) then False else if len >= 8 then True else isStartTwo
+    where
+        len = length middleHai
+        middleHai = getMiddleHai hai
+        reverseMiddleHai = reverse middleHai
+        isStartTwo = (length . removeHeadZero $ hai) == 8
+
+-- 重心の位置が真ん中含めて前にあるか
+isFormerGravity :: [Int] -> [Int] -> Bool
+isFormerGravity [] [] = True
+isFormerGravity [] _ = True
+isFormerGravity _ [] = True
+isFormerGravity (x:xs) (y:ys) = if x > y then True else if x < y then False else isFormerGravity xs ys
+
+
+-- 両端の 0 続きを除いたリスト
+getMiddleHai :: [Int] -> [Int]
+getMiddleHai = removeHeadZero . reverse . removeHeadZero
+
+-- 先頭から初めて 0 ではないものが出てくるまで除去する
+removeHeadZero :: [Int] -> [Int]
+removeHeadZero [] = []
+removeHeadZero (x:xs) = if x == 0 then removeHeadZero xs else (x:xs)
+
+
+
+
+
 isTargetHai :: [Int] -> Bool
-isTargetHai hai = (not . GetHai.isExistOverHaiCount) hai && isTenpai hai && isSemiIrreducible hai && isIrreducible hai 
+isTargetHai hai = (not . GetHai.isExistOverHaiCount) hai && isNormalForm hai && isTenpai hai && isSemiIrreducible hai && isIrreducible hai
 
 -- main = print $ GetHai.nextHai [0, 0, 3]
 -- main = print $ Agari.isAgariForm 0 1 [1, 0, 2] 
