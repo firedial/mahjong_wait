fb3c80923323c6cbe4b97276ab3636991816e5e3
firedial <42674772+firedial@users.noreply.github.com>
2021-01-16 16:46:13 +0900

    使いやすいように修正

--- main.hs
+++ main.hs
@@ -4,26 +4,23 @@ import qualified Agari
 import qualified NormalForm
 import qualified Irreducible
 
-isTargetHai :: [Int] -> Bool
-isTargetHai hai = (not . GetHai.isExistOverHaiCount) hai && NormalForm.isNormalForm hai && Agari.isTempaiForm hai && Irreducible.isSemiIrreducible hai && Irreducible.isIrreducible hai
+isTargetForm :: [Int] -> Bool
+isTargetForm hai = and $ map (\f -> f hai) checkFunctions
+    where
+        checkFunctions = if isRegularForm then baseCheckFunctions ++ [Irreducible.isIrreducible] else baseCheckFunctions
+        isRegularForm = mod (sum hai) 3 == 1
+        baseCheckFunctions = [(not . GetHai.isExistOverHaiCount), NormalForm.isNormalForm, Agari.isTempaiForm, Irreducible.isSemiIrreducible]
 
-isTargetHaiRemoveAtama :: [Int] -> Bool
-isTargetHaiRemoveAtama hai = (not . GetHai.isExistOverHaiCount) hai && NormalForm.isNormalForm hai && Agari.isTempaiForm hai && Irreducible.isSemiIrreducible hai
+main = do
+    n <- readLn
+    getList n 
 
--- main = print $ GetHai.nextHai [0, 0, 3]
--- main = print $ Agari.isAgariForm 0 1 [1, 0, 2] 
--- main = print $ isNonRelatedAtama [0, 2, 0, 0, 0, 0, 1, 1, 3]
--- main = print $ (map getAgariHai (Agari.removeMentsuPossibleFromList [[0, 2, 2, 0, 3, 0, 0, 0, 0]]) )
-
--- main = print $ isIrreducible [0, 3, 2, 2, 0, 0, 0, 0, 0]
-main = printResult [0, 0, 0, 0, 0, 0, 0, 0, 10]
+getList :: Int -> IO()
+getList n =
+    printResult [0, 0, 0, 0, 0, 0, 0, 0, n]
 
 printResult :: [Int] -> IO ()
 printResult hai = do
-    -- if isTargetHaiRemoveAtama hai then print hai else return ()
-    if isTargetHai hai then print hai else return ()
-    if hai == [10, 0, 0, 0, 0, 0, 0, 0, 0] then return () else printResult $ GetHai.nextHai hai
-
-
-
+    if isTargetForm hai then print hai else return ()
+    if head hai == sum hai then return () else printResult $ GetHai.nextHai hai
 
